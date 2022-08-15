6bd059e5a0f87393fde42616d604f68b042ab24a
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2022-08-15 15:47:18 +0200

    Init

---
+++ DLVideo/popup.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('input');
+
+if (input == "cmp3"){
+    URL= getCurrentUrl;
+    dlInMp3;
+}
+
+if (input == "cmp4"){
+    URL= getCurrentUrl;
+    dlInMp4;
+}
+
+async function getCurrentUrl(){
+    Url = browser.tabs.URL;
+    testURL = "https://www.youtube.com/watch?v=QH2-TGUlwu4&t=1s";
+    return Url;
+}
+
+async function dlInMp3(URL){
+
+}
+
+async function dlInMp4(URL){
+
+}
