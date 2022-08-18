0cf81c122a9e39e3613134cb28736248ae4c8fbe
Stevehunn <nicolas.bitaillou@etu.univ-tours.fr>
2022-08-18 02:24:10 +0200

    reset

---
+++ VideoDL/background.js
@@ -0,0 +1,10 @@
+console.log("hello l'extension charge ce script")
+
+function getValue() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    var input = document.getElementById("in").value;
+
+    // Afficher la valeur
+    alert(input);
+}
+
